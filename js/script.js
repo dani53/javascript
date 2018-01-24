2c578b9fe72e79dd69b383c472a3cd1347c06c42
MacBook Air <danielle53@hotmail.de>
2018-01-23 19:03:48 -0800

    day 2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+let firstName = prompt(“What is your first name?”, “Maria”);
+const lastName = prompt (“Wheat is your last name?”, “is..”);
+console.log(firstName);
+alert(lastName);
+Let birthday = prompt(“What’s your birthday?”, “Please answer in the following format: 1/1/1900”);
+confirm(new Date(birthday));
