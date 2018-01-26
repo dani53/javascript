95213672c398b3cb18564f4493eebdd995d8a4e3
MacBook Air <danielle53@hotmail.de>
2018-01-25 19:52:20 -0800

    finish day 3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,80 @@
-console.log("Hello World");
-let firstName = prompt(“What is your first name?”, “Maria”);
-const lastName = prompt (“Wheat is your last name?”, “is..”);
-console.log(firstName);
-alert(lastName);
-Let birthday = prompt(“What’s your birthday?”, “Please answer in the following format: 1/1/1900”);
-confirm(new Date(birthday));
-/*whatever I want*/
+//*console.log("Hello World");*//
+//*let firstName = prompt(“What is your first name?”, “Maria”);*//
+//*const lastName = prompt (“Wheat is your last name?”, “is..”);*//
+//*console.log(firstName);*//
+//*alert(lastName);*//
+//*Let birthday = prompt(“What’s your birthday?”, “Please answer in the following format: 1/1/1900”);*//
+//*confirm(new Date(birthday));*//
+//*whatever I want*//
+
+
+//*console.log(!bestNumber);*//
+
+//*var bestNumber = 42*//
+
+
+//*var number = 1;*//
+//*console.log(string === number);*//
+
+//*let obj1 = {name:"Ryeker"};*//
+//*let obj2 = {name:"Ryeker"};*//
+
+
+//*var = 1;*//
+//*var newString "1";*//
+
+
+//*let*//
+
+// const bestMovie = [];
+// bestMovie !== undefined ? alert('it exists!') : alert('It doesnt exist!');
+// if(typeof(bestMovie) == 'string'){
+// alert('yay its a string');
+// } else if(typeof(bestMovie) == 'number'){
+//   alert('you gave me a number')
+// } else {
+//   alert('Its not a string')
+// }
+
+// switch(typeof(bestMovie)) {
+  // case 'string':
+      // alert('Yay it\'s a string');
+      // break;
+  // case 'number':
+      // alert('You gave a number');
+      // break;
+  // case 'boolean':
+      // alert("You gave me a boolean");
+      // break;
+  // case 'object':
+      // alert('an object!')
+      // break;
+    // default:
+      // alert('its and array');
+      // break;
+// }
+
+// var userAge = confirm('Are you over 18?', 'no');
+
+// if(userAge >= 18) {
+  // console.log('it worked');
+  // do stuff
+// } else {
+  // alert('You are not old enough...');
+// }
+
+var userAge = confirm('Are you over 18?');
+
+if(userAge == true) {
+  under80 = confirm('Are you under 80 yeasrs old?');
+  if(under80) {
+    var starWars = confirm("Do you like star wars?");
+    if(starWars) {
+      alert('you rock!');
+      }
+} else {
+  confirm('Do you like prunes?');
+  }
+} else {
+  alert('You are not old enough...');
+}
