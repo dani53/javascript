21a7bb7436975c88a3969ee17b55027340657236
MacBook Air <danielle53@hotmail.de>
2018-02-08 18:06:37 -0800

    unfinished take home

--- js/script.js
+++ js/script.js
@@ -62,3 +62,62 @@
 // for(let i = 0; i < sortedWithIndices.length; i++) {
   // if(sortedWithIndices[i][1]);
 // }
+
+//Fat arrow functions
+// function callMyself(number) {
+//   if(number >=0) {
+//     number--;
+//     console.log(number);
+//   } else {
+//     return
+//   }
+// }
+//
+// (number) => {
+//   if(number >= 0) {
+//     number--;
+//     console.log(number);
+//   } else
+// }
+// function givesMeBestNumber(number) {
+//   if(number)
+// }
+
+// function dogsAge(number) {
+//   if(number )
+// }
+//Try it out day5
+//const dogAge = prompt("how old is your dog?", '3');
+// need a function to take that step and do something ith it
+// This is a declaration..definition of the function - what matters is where we call the function
+// function calcDogYears(age) {
+//   parseInt(age); // check that it is a givesMeBestNumber
+//   // if(age == NaN) { //nan is an object - not saved by their value, but where they are saved
+//     if(isNaN(age)) { //a function called isNan will calculate if someghing is not a number
+//       //if age comes back as not a number
+//       calcDogYears(prompt(`Nah for real..NUMBER`, `10`));
+//     } else {  //track how many times the question has been asked -extra cred!
+//       alert(age *=7); //age equals age times 7
+//   }
+// }
+//
+// calcDogYears(dogAge);
+
+// const lifeTimeSupply = prompt("How old are you now?", "20");
+// parseInt(age);
+//   if(age == NaN) {
+//     if(isNan(age))
+//   }
+
+// function calcLifetimeSupply(age, perDay) {
+//   return (80 - age) * (365 * perDay);
+//   let testArr = [age, perDay];
+//   if(testForInts(testArr)){
+//     var lifeTime = (80 - age) * (365 perDay);
+//     alert(`uyou will need ${lifeTime} ${supplyOf} to last until you're 80.`);
+//   } else {
+//     var age = prompt("How old are you?");
+//     var daily = prompt("How much do you use per day?");
+//     lifeTime = calcLifetimeSupply(age, daily);
+//   }
+// }
